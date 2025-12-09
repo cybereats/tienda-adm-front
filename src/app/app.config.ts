217754a89a615e8266576f3bdd2cf48db50b746e
Ismael Monleón Rubio <ismael.monleon73@gmail.com>
2025-12-09 18:11:50 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
+
+registerLocaleData(localeEs);
 
 import { routes } from './app.routes';
 
@@ -7,6 +12,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
   ]
 };
